dc38d358bf8a08fc8cc61efd9e447c84c3281f82
Levon Stepanyan <levonmll@gmail.com>
2026-08-30 23:51:53 +0300

    ex 1.7 ingress

--- log_outputs/index.js
+++ log_outputs/index.js
@@ -1,8 +1,22 @@
 import crypto from 'crypto'
+import express from 'express'
+
+const app = express()
+let currentStatus
+
+app.get('/', (req, res) => {
+    res.send(currentStatus)
+})
+
 
 const randomHash = crypto.randomUUID();
 const getHashNow = () => {
-    console.log(new Date().toISOString(), randomHash)
+    const currentTime = new Date().toISOString()
+    console.log(currentTime, randomHash)
+    currentStatus = `${currentTime} ${randomHash}`
     setTimeout(() => getHashNow(), 5000)
 }
 getHashNow()
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
