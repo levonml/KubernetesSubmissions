7a4cf4fd219494d41a0e05db5c1d7fcea3d016d3
Levon Stepanyan <levonmll@gmail.com>
2026-09-09 01:17:04 +0300

    ex 2.2 update

--- README.md
+++ README.md
@@ -21,3 +21,4 @@
 ### Chapter 3
 
  - [2.1](https://github.com/levonml/KubernetesSubmissions/tree/2.1/log_outputs)
+ - [2.2](https://github.com/levonml/KubernetesSubmissions/tree/2.2/the_project)
---
+++ todo-backend/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const todos = [];
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/todos', (req, res) => {
+    res.status(200).json({ todos });
+});
+
+app.post('/todos', (req, res) => {
+    console.log(`Received todo in the backend: ${JSON.stringify(req.body)}`);
+    const { data } = req.body;
+    // Here you would typically add the todo item to your database
+    todos.push(data);
+    res.status(200).json({ todos });
+});
