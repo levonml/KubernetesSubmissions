0016ed5952ae96290e0a02b3fb249a8ca46ef170
Levon Stepanyan <levonmll@gmail.com>
2026-09-10 18:36:52 +0300

    ex 2.5

--- README.md
+++ README.md
@@ -24,3 +24,4 @@
  - [2.2](https://github.com/levonml/KubernetesSubmissions/tree/2.2/the_project)
  - [2.3](https://github.com/levonml/KubernetesSubmissions/tree/2.3)
  - [2.4](https://github.com/levonml/KubernetesSubmissions/tree/2.4)
+ - [2.5](https://github.com/levonml/KubernetesSubmissions/tree/2.5/log_outputs)
--- log_outputs/string-reader/index.js
+++ log_outputs/string-reader/index.js
@@ -3,7 +3,11 @@ import express from 'express'
 import fs from 'fs'
 import path from 'path'
 import axios from 'axios'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const myMessage = process.env.MESSAGE
 async function getPongs() {
     try {
         const response = await axios.get('http://pong-o-svc:2346/pings')
@@ -18,9 +22,12 @@ const app = express()
 let currentStatus
 
 const logFilePath = '/tmp/kube/logoutput.txt'
+const informationFilePath = '/etc/kube/configmap/information.txt'
 const pongCounterFilePath = '/tmp/kube/pongcounter.txt'
-fs.mkdirSync(path.dirname(logFilePath), { recursive: true })
+//fs.mkdirSync(path.dirname(logFilePath), { recursive: true })
 //fs.mkdirSync(path.dirname(pongCounterFilePath), { recursive: true })
+
+const information = fs.readFileSync(informationFilePath, 'utf-8')
 
 setInterval(() => {
     currentStatus = fs.readFileSync(logFilePath, 'utf-8')
@@ -30,7 +37,7 @@ app.get('/showlogs', async (req, res) => {
         let pongCounter = await getPongs()
         // const pongCounter = fs.readFileSync(pongCounterFilePath, 'utf-8')
         console.log("currentStatus:", currentStatus)
-        res.send(`${currentStatus}.<br>Ping / Pongs: ${pongCounter}`)
+        res.send(`file content: ${information}<br>env variable: ${myMessage}<br>${currentStatus}.<br>Ping / Pongs: ${pongCounter}`)
     } catch (error) {
         console.error('Error handling /showlogs request:', error)
         res.status(500).send('Internal Server Error')
